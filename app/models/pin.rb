14e9ffbe44329ceea190d20526e6a85b7aac1e5b
enzo <enzuu@hotmail.com>
2015-09-20 18:26:43 -0300

    add gem for s3amazon for real

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,11 @@
 class Pin < ActiveRecord::Base
    belongs_to :user
    acts_as_votable
-   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+   has_attached_file :image,
+                     :style => { :medium => "300x300>", :thumb => "100x100>" },
+                     :storage => :s3,
+                     :bucket  => ENV['MY_BUCKET_NAME']
+   validates :image, presence: true
+   validates :description, presence: true
 end
 
